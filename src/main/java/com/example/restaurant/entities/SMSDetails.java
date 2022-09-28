daf4d72706ff0c3529e260722db81b7a1590ea30
jiaswang <jiaswang@student.unimelb.edu.au>
2022-09-28 11:55:46 +1000

    Implement SMS functionality

---
+++ src/main/java/com/example/restaurant/entities/SMSDetails.java
@@ -0,0 +1,33 @@
+package com.example.restaurant.entities;
+
+public class SMSDetails {
+
+        private String to;
+        private String message;
+
+        public String getTo() {
+            return to;
+        }
+
+        public void setTo(String to) {
+            this.to = to;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+
+        @Override
+        public String toString() {
+            return "SMS{" +
+                    "to='" + to + '\'' +
+                    ", message='" + message + '\'' +
+                    '}';
+        }
+
+}
